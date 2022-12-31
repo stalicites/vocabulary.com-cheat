e05e0d3830adf73afdad3f441a78df4a42ae8216
Stalicites <88872425+stalicites@users.noreply.github.com>
2022-12-30 23:53:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-let instructionElementList = document.getElementsByClassName("instructions");
-let finalElement = instructionElementList[instructionElementList.length - 1];
-let word = finalElement.getElementsByTagName("strong")[0].innerText
-
-let apis = [`https://vocabulary.vercel.app/word`,`https://vocabulary.vercel.app/words`];
-let requests = [];
-let dataResponses = []
-
-for (let i = 0; i < apis.length; i++) {
-    let q = new XMLHttpRequest();
-    q.open("GET", `${apis[i]}/${word}`);
-    requests.push(q);
-    q.onload = () => {
-        console.log("data received!");
-        dataResponses.push(JSON.parse(q.responseText));
+document.onkeydown = (e) => {
+    if (e.key == "/") {
+        let instructionElementList = document.getElementsByClassName("instructions");
+        let finalElement = instructionElementList[instructionElementList.length - 1];
+        let word = finalElement.getElementsByTagName("strong")[0].innerText
+        
+        localStorage.getItem("dict") ? 0 : localStorage.setItem("dict", "{}");
+        
+        let apis = [`https://vocabulary.vercel.app/word`,`https://vocabulary.vercel.app/words`];
+        let requests = [];
+        let dataResponses = []
+        
+        for (let i = 0; i < apis.length; i++) {
+            let q = new XMLHttpRequest();
+            q.open("GET", `${apis[i]}/${word}`);
+            requests.push(q);
+            q.onload = () => {
+                console.log("data received! checking & saving to dictionary");
+                let data = JSON.parse(q.responseText).data;
+                if (typeof data == "string") {
+                     dataResponses.push(data);   
+                } else if (typeof data == "object") {
+                    if (data.length) {
+                         data.forEach((item) => {
+                             dataResponses.push(item.description)
+                         })   
+                    } else {
+                        console.log("some weird data :/", data);
+                    }
+                }
+                if (i == apis.length - 1) {
+                    console.log("we're at the end");
+                    console.log("data responses: ", dataResponses)
+                    alert(`The definition of ${word} is: \n${dataResponses}`)
+                }
+            }
+            q.send();
+        }
+        
     }
-    q.send();
 }
