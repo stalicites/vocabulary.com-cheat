9aec90ddbfdde682f192cd1e5c2ed1c924b483e3
Stalicites <88872425+stalicites@users.noreply.github.com>
2023-02-11 20:49:55 -0800

    Update v2.js

--- v2.js
+++ v2.js
@@ -2,7 +2,26 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-document.onkeydown = async (e) => {
+async function guessIfNoAnswer(choices, q, storedData) {
+    for (let i = 0; i < choices.length; i++) {
+        if (choices[i]) {
+            choices[i].click();
+            await sleep(1000);
+            console.log("Done!");
+        }
+    }
+    let answerText = document.getElementsByClassName("correct is-disabled")[0].innerText;
+    if (storedData[q]) {
+        storedData[q].push(answerText);
+    } else {
+        storedData[q] = [answerText]
+    }
+    console.log(answerText);
+    localStorage.setItem("dict", JSON.stringify(storedData));
+    document.getElementsByClassName("btn-next")[0].click();
+}
+
+document.onkeydown = (e) => {
     if (e.key == "/") {
         let instructionElementList;
         if (document.getElementsByClassName("instructions").length == 0) {
@@ -15,23 +34,28 @@ document.onkeydown = async (e) => {
         localStorage.getItem("dict") ? 0 : localStorage.setItem("dict", "{}");
         
         let storedData = JSON.parse(localStorage.getItem("dict"));
+        let choices = document.getElementsByClassName("choices")[0].getElementsByTagName("a")
         if (storedData[q]) {
             console.log("Question exists");
-            alert(storedData[q]);
-        } else {
-            let choices = document.getElementsByClassName("choices")[0].getElementsByTagName("a")
+            let finished = false;
             for (let i = 0; i < choices.length; i++) {
-                if (choices[i]) {
-                    choices[i].click();
-                    await sleep(1000);
-                    console.log("Done!");
+                let choice = choices[i]
+                if (choice.innerText == storedData[q]) {
+                    choice.click();
+                    finished = true;
+                    break;
                 }
             }
-            let answerText = document.getElementsByClassName("correct is-disabled")[0].innerText;
-            storedData[q] = answerText
-            console.log(answerText);
-            localStorage.setItem("dict", JSON.stringify(storedData));
-            document.getElementsByClassName("btn-next")[0].click();
+            if (!finished) {
+                console.log("there's a second answer!");
+                guessIfNoAnswer(choices, q, storedData);    
+                storedData[q].push()
+            } else {
+                document.getElementsByClassName("btn-next")[0].click();
+            }
+            console.log("answer: ", storedData[q]);
+        } else {
+            guessIfNoAnswer(choices, q, storedData);
         }
     }
 }
